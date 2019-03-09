3f247e093c946435879fed64fc4cb45aa2819fb5
LeoCaronezi <rafael.caronezi@outlook.com>
2019-03-09 14:01:54 -0300

    LeoCaronezi
    criando as variaveis e atributos

--- CalcularoraAritimetrica/app/src/main/java/com/e/calcularoraaritimetrica/MainActivity.java
+++ CalcularoraAritimetrica/app/src/main/java/com/e/calcularoraaritimetrica/MainActivity.java
@@ -2,12 +2,60 @@ package com.e.calcularoraaritimetrica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+     private EditText  num1;
+     private EditText  num2;
+     private Button    soma;
+     private Button   divisao;
+     private Button   subtracao;
+     private Button   mutiplicacao;
+     private TextView resultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        num1 = (EditText) findViewById (R.id.ID_NUM1editText);
+        num2 = (EditText) findViewById (R.id.ID_NUM2editText2);
+        soma = (Button)   findViewById (R.id.ID_SOMAbutton);
+        subtracao = (Button) findViewById (R.id.ID_SOMAbutton);
+        mutiplicacao =(Button) findViewById((R.id.ID_MUTIPLICACAObutton3);
+        divisao = (Button) findViewById(R.id.ID_DIVISAObutton4);
+        resultado = (TextView) findViewById(R.id.ID_RESULTADOtextView3);
+
+
+        soma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+         subtracao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+          mutiplicacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+         divisao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 }
