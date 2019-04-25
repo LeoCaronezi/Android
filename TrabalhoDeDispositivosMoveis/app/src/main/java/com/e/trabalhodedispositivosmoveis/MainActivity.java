b690d21875f17101e58405f6c3b8572cf81240a3
LeoCaronezi <rafael.caronezi@outlook.com>
2019-04-25 13:15:54 -0300

    LeoCaronezi
    criação dos primeiros componentes do trabalho.

--- TrabalhoDeDispositivosMoveis/app/src/main/java/com/e/trabalhodedispositivosmoveis/MainActivity.java
+++ TrabalhoDeDispositivosMoveis/app/src/main/java/com/e/trabalhodedispositivosmoveis/MainActivity.java
@@ -2,12 +2,22 @@ package com.e.trabalhodedispositivosmoveis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nomeDoProduto;
+    private EditText codigoDoProduto;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nomeDoProduto = (EditText) findViewById(R.id.NomeDoProdutoeditText);
+        codigoDoProduto =(EditText) findViewById(R.id.CodigoDoProdutoeditText2);
+
     }
 }
