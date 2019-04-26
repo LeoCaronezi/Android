005083567714aef9123ca0df23dd9cafa4492995
LeoCaronezi <rafael.caronezi@outlook.com>
2019-04-26 17:15:59 -0300

    LeoCaronezi
    removendo os componten criado anteriores e criando novos

--- TrabalhoDeDispositivosMoveis/app/src/main/java/com/e/trabalhodedispositivosmoveis/MainActivity.java
+++ TrabalhoDeDispositivosMoveis/app/src/main/java/com/e/trabalhodedispositivosmoveis/MainActivity.java
@@ -8,10 +8,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText nomeDoProduto;
-    private EditText codigoDoProduto;
-    private Button comprar;
-    private Button vender;
+    private EditText material;
+    private EditText peso;
 
 
 
@@ -20,25 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nomeDoProduto = (EditText) findViewById(R.id.NomeDoProdutoeditText);
-        codigoDoProduto =(EditText) findViewById(R.id.CodigoDoProdutoeditText2);
-        comprar = (Button) findViewById(R.id.Comprabutton);
-        vender = (Button) findViewById(R.id.Venderbutton2);
-
-
-        comprar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        vender.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+   
     }
 
 }
