0568bf3228afdc7f08503202d442fb14e9f5feb1
LeoCaronezi <rafael.caronezi@outlook.com>
2019-04-26 14:05:04 -0300

    LeoCaronezi
    criando  os compra.setOnclick e o vender.setonClikci

--- TrabalhoDeDispositivosMoveis/app/src/main/java/com/e/trabalhodedispositivosmoveis/MainActivity.java
+++ TrabalhoDeDispositivosMoveis/app/src/main/java/com/e/trabalhodedispositivosmoveis/MainActivity.java
@@ -2,6 +2,7 @@ package com.e.trabalhodedispositivosmoveis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,7 +25,20 @@ public class MainActivity extends AppCompatActivity {
         comprar = (Button) findViewById(R.id.Comprabutton);
         vender = (Button) findViewById(R.id.Venderbutton2);
 
-        
+
+        comprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        vender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
+
 }
