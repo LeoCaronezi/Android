e5b841e2c6316c641cbaa1f3dd97296e2ed086c6
LeoCaronezi <rafael.caronezi@outlook.com>
2019-05-05 13:58:28 -0300

    LeoCaronezi
    comecando a declara os componentes

--- Exemplo3/app/src/main/java/com/e/exemplo3/MainActivity.java
+++ Exemplo3/app/src/main/java/com/e/exemplo3/MainActivity.java
@@ -2,12 +2,48 @@ package com.e.exemplo3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox checkBox1;
+    private CheckBox checkBox2;
+    private CheckBox checkBox3;
+    private CheckBox checkBox4;
+    private Button   exibir;
+    private Button   desmarcar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkBox1 = (CheckBox) findViewById(R.id.checkBox);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+        checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
+        exibir = (Button) findViewById(R.id.Exibir);
+        desmarcar = (Button) findViewById(R.id.Desmarcar);
+
+        //fazendo  o controle de enventos dos checkbox
+
+
+
+        exibir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        desmarcar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
